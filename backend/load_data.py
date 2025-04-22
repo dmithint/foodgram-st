ccb2ef519f90773854d84e7a0cedd54d830b0a21
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2025-04-23 02:17:49 +0300

    lint fix

--- backend/load_data.py
+++ backend/load_data.py
@@ -1,11 +1,12 @@
 import os
 import json
 import django
+from recipes.models import Ingredient
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram_backend.settings')
 django.setup()
 
-from recipes.models import Ingredient
 
 def load_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as jsonfile:
@@ -13,9 +14,11 @@ def load_json(file_path):
         for item in data:
             Ingredient.objects.get_or_create(**item)
 
+
 def main():
     file_path = os.getenv('DATA_FILE', 'data/ingredients.json')
     load_json(file_path)
+
 
 if __name__ == '__main__':
     main()
